366601f649f3a828620df8738a0f2558b5b80e3f
mohamed-adel-30 <a.adel9@outlook.com>
2020-12-14 22:57:27 +0200

    Weather App

---
+++ website/app.js
@@ -0,0 +1,86 @@
+/* Global Variables */
+let zipCode = document.getElementById('zip')
+let feeling = document.getElementById('feelings')
+let formSubmitWeather = document.getElementById('submitForm');
+
+
+// Sumbit form ..
+formSubmitWeather.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+    let objData = {
+        zipCode: zipCode.value,
+        feeling: feeling.value,
+    };
+
+    // All Api Information
+    let apiKey = 'abfdb6607d48d78c9bc9e74abfa87f0e';
+    console.log(apiKey);
+    // Get Api Weather App With My api Key
+    let apiInformation = async function() {
+        await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${objData.zipCode},us&appid=${apiKey}`, {
+                method: "GET"
+            })
+            .then(res => res.json())
+            .then(data => {
+                
+                
+                let objPost = {
+                    temp: data.main.temp,
+                    date: newDate,
+                    feeling: objData.feeling
+                }
+
+                postAndGetData(objPost);
+               
+            })
+            .catch(err => console.log("err", err))
+    }
+    apiInformation();
+    // Post Data in server 
+
+
+})
+
+
+
+function postAndGetData(obj){
+
+     // Post Data to server.js
+     let addData = async function() {
+        return await fetch(`http://localhost:8000/postData`, {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(obj)
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+
+    }
+    
+    // Get Data from server.js
+    const getData = async function() {
+        let showData = []
+        return await fetch(`http://localhost:8000/getData`)
+            .then(res => res.json())
+            .then(data => {
+                showData += `
+                    <div class="data">
+                    <div > Date : <i class="far fa-calendar-alt"></i> ${data.date}</div>
+                    <div > Temperature : <i class="fas fa-cloud-sun"></i> ${data.temp}</div>
+                    <div > My Feeling : ${data.feeling}</div>
+                    </div>
+                `
+                document.getElementById('entryHolder').innerHTML = showData
+            })
+            .catch(err => console.log("err", err))
+    }
+
+    addData();
+    getData();
+}
+
